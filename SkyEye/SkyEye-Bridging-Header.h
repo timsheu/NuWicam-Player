295a93ff1a4db5070b2f2341723db4ad99437e11
timsheu <timsheu.ash@gmail.com>
2016-10-05 13:29:06 +0800

    Change player to kxmovie

--- SkyEye/SkyEye-Bridging-Header.h
+++ SkyEye/SkyEye-Bridging-Header.h
@@ -2,3 +2,6 @@
 //  Use this file to import your target's public headers that you would like to expose to Swift.
 //
 
+#include "PlayerManager.h"
+#include "SocketManager.h"
+#include "KxMovieViewController.h"
---
+++ SkyEye/StartViewController.swift
@@ -0,0 +1,53 @@
+//
+//  StartViewController.swift
+//  NuWicam
+//
+//  Created by CCHSU20 on 10/5/16.
+//  Copyright © 2016 Nuvoton. All rights reserved.
+//
+
+import UIKit
+
+class StartViewController: UIViewController {
+    let TAG = "StartViewController:"
+    var playerManager: PlayerManager?
+    var cameraURL: String?
+    
+    //MARK: override UIViewController functions
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        playerManager = PlayerManager.sharedInstance()
+        let dic = playerManager?.dictionarySetting["Setup Camera"] as! NSDictionary
+        if let URL = dic["URL"]{
+            cameraURL = URL as? String
+        }
+        print("\(TAG) camera URL: \(cameraURL)")
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    //MARK: IBActions
+    @IBAction func enterLive(){
+        if cameraURL != nil {
+            let parameters: NSMutableDictionary = NSMutableDictionary.init(capacity: 1)
+            parameters[KxMovieParameterDisableDeinterlacing] = true
+            let kxmovie = KxMovieViewController.movieViewControllerWithContentPath(cameraURL, parameters: parameters as [NSObject : AnyObject])
+            presentViewController(kxmovie as! KxMovieViewController, animated: true, completion: nil)
+        }
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
